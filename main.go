fbad31872369b1b20cd2927ced9b6c4f35f4ec89
Ranjan, Nishit (US - Bengaluru) <nishiranjan@deloitte.com>
2018-10-21 13:51:14 +0530

    first commit

--- main.go
+++ main.go
@@ -14,5 +14,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	//making a byte slice
+	//make is a built in function that takes a type of slice and the second argument is empty space that
+	//you want the slice to be initialized with. if we ,make it 0, Read function will assume that the slice is already full. so 99999 is kind of a guess about the empty space that would be needed for the slice
+	bs := make([]byte, 99999)
+
+	//the response body takes the byte slice and passes it to the Read function
+	resp.Body.Read(bs)
+
+	fmt.Println(string(bs))
+
 }
